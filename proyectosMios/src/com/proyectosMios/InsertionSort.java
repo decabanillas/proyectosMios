1c5fd8abc2b5d96492da7b07b4469c23abf1bf16
Vaio <Vaio@Vaio-PC>
2017-08-07 12:51:13 -0500

    final

--- proyectosMios/src/com/proyectosMios/InsertionSort.java
+++ proyectosMios/src/com/proyectosMios/InsertionSort.java
@@ -1,27 +1,44 @@
 package com.proyectosMios;
 
 import java.util.Scanner;
-import java.util.ArrayList;
-import java.util.List;
+
 
 public class InsertionSort {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		int a;
+		int v,j,temp=0;
 		int [] lista = new int[5];
 		System.out.println("ingrese la lista de numeros: ");
 		Scanner sc = new Scanner (System.in);
 		
 		for(int i=0;i<lista.length;i++) {
 		
-			a=sc.nextInt();
-			lista[i]=a;	
+			lista[i]=sc.nextInt();
+			
 		}
 		System.out.println("los numeros en la lista son");
-		for(int j=0;j<lista.length;j++) {
-			System.out.print(" "+ lista[j]);
+		for(int i=0;i<lista.length;i++) {
+			System.out.print(" "+ lista[i]);
 		}
+		
+		for(int i =1; i<lista.length;i++ ) {
+			v=lista[i];
+			j=i-1;
+			while ( j>=0 && v<lista[j]) {
+				temp=lista[j];
+				lista[j]=lista[j+1];
+				lista[j+1]=temp;
+				j--;
+			}
+			//lista[j]=v;
+		}
+		System.out.println(" ");
+		System.out.println("la lista ordenada es: ");
+		for(int i=0;i<lista.length;i++) {
+			System.out.print(" "+lista[i]);
+		}
+		
 	}
 
 }
