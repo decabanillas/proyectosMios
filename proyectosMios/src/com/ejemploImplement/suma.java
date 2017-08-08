6ea16132bd838d0ed060ff5671e8c5fcb42b08a5
Escritorio <Escritorio@>
2017-08-08 10:59:54 -0500

    probe la multiherencia, llamando una interface de otro paquete, mirar como puse otra al final, q pertenece a com.herencia public class suma implements sumarCodigo,otraOperacion,otra

--- proyectosMios/src/com/ejemploImplement/suma.java
+++ proyectosMios/src/com/ejemploImplement/suma.java
@@ -1,7 +1,8 @@
 package com.ejemploImplement;
+import com.herencia.otra;
 
 //el implement es como una multiple herencia AQUIIII OTRA OPERACION MULTIHERENCIA
-public class suma implements sumarCodigo,otraOperacion{
+public class suma implements sumarCodigo,otraOperacion,otra{
 	int a,b;
 	public suma(){
 		this.a=5;
@@ -22,12 +23,21 @@ public class suma implements sumarCodigo,otraOperacion{
 		System.out.println("suma de variables de instancia: "+(this.a-this.b) );
 		return 0;
 	}
-	
+	//este viene de herencia, la interface "otra" de otro paqueete(com.herencia)
+	@Override
+	public void otro() {
+		// TODO Auto-generated method stub
+		System.out.println("Yo vengo de com.herencia");
+		
+	}
 	public static void main(String []args) {
 		suma sum =new suma();
 		System.out.println(sum.sumar(10, 8));
 		System.out.println(sum.resta(10, 8));
+		sum.otro();
 	}
+
+	
 
 	
 	
---
+++ proyectosMios/src/com/herencia/otra.java
@@ -0,0 +1,5 @@
+package com.herencia;
+//Esto lo hice para llamarlo desde com.ejemploImplement de otro paquete, no lo use en la herencia camion, auto
+public interface otra {
+	public abstract void otro();
+}
