6f443c97d877539ee121b356f4537e8568e48703
Escritorio <Escritorio@>
2017-08-08 10:51:00 -0500

    agregado el multiherencia

---
+++ proyectosMios/src/com/ejemploImplement/otraOperacion.java
@@ -0,0 +1,5 @@
+package com.ejemploImplement;
+
+public interface otraOperacion {
+	public abstract  int resta(int a, int b);
+}
--- proyectosMios/src/com/ejemploImplement/suma.java
+++ proyectosMios/src/com/ejemploImplement/suma.java
@@ -1,7 +1,7 @@
 package com.ejemploImplement;
 
-//el implement es como una multiple herencia
-public class suma implements sumarCodigo{
+//el implement es como una multiple herencia AQUIIII OTRA OPERACION MULTIHERENCIA
+public class suma implements sumarCodigo,otraOperacion{
 	int a,b;
 	public suma(){
 		this.a=5;
@@ -15,9 +15,20 @@ public class suma implements sumarCodigo{
 		System.out.println("suma de variables de instancia: "+(this.a+this.b) );
 		return 0;
 	}
+	@Override
+	public int resta(int a, int b) {
+		// TODO Auto-generated method stub
+		System.out.println("suma de variables locales: "+(a-b) );
+		System.out.println("suma de variables de instancia: "+(this.a-this.b) );
+		return 0;
+	}
+	
 	public static void main(String []args) {
 		suma sum =new suma();
 		System.out.println(sum.sumar(10, 8));
+		System.out.println(sum.resta(10, 8));
 	}
+
+	
 	
 }
