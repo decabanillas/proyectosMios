9552ba6e7845a0ac0d15e20fadb08a47ac63342d
HP <HP@HP-PC>
2017-08-11 11:21:40 -0500

    ejemplo con streambuffer

--- proyectosMios/src/miStream/miStream.java
+++ proyectosMios/src/miStream/miStream.java
@@ -1,30 +1,48 @@
 package miStream;
 import java.io.FileReader;
 import java.io.IOException;
+import java.io.BufferedReader;
 
 public class miStream {
 	//flujo de bytes imputStream, OutputStream(informacion en bytes)
 	//secuencia de caracteres reader writer
-	
+	FileReader entrada;
 			public miStream(){
 				
 				
 			}
+			
 				public void lee() {
 					try {
-						FileReader entrada =new FileReader("C:/Users/Escritorio/Desktop/tareas.txt");
+						entrada =new FileReader("C:/Users/HP/Documents/ejemplo.txt");
+						BufferedReader mibufer= new BufferedReader(entrada);
+						//fileImputStream abre cualquier tipo de archivo
+						/*//esto antes del buffer (el buffer es mas optimo para mucho texto)
 						int c=0;
 						//-1 indica el final
 						while(c!=-1) {
 							c=entrada.read();
 							char letra = (char)c;
-							System.out.print(letra);
+							System.out.print(letra);*/
+							String linea="";
+								while(linea!=null) {
+									linea=mibufer.readLine();
+									if(linea!=null) {
+										System.out.println(linea);
+									}
+								}
 							
 							
-						}
-						entrada.close();
+						
+						//
 					}catch(IOException e) {
-						System.out.println("no se encuentra la imagen");
+						System.out.println("no se encuentra el texto");
+					}finally {
+						try {
+							entrada.close();
+						}catch(IOException e) {
+							System.out.println("exepcion de finally");
+						}
 					}
 				
 				
